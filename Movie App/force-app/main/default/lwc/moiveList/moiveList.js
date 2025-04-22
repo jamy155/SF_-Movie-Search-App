dc209ada6d4e11659319799a33464fdfa2800b0d
jamy155 <jarhudaugustin@gmail.com>
2025-04-22 08:04:05 -0400

    UPDATED movieList component

--- Movie App/force-app/main/default/lwc/moiveList/moiveList.js
+++ Movie App/force-app/main/default/lwc/moiveList/moiveList.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class MoiveList extends LightningElement {}
+export default class MoiveList extends LightningElement {
+
+    enteredText='';
+    searchText='';
+    showText= 'Please enter Valid Movie Name';
+
+    handleChange(event){
+        this.enteredText = event.target.value;
+    }
+
+    handleClick(event){
+        this.searchText = this.enteredText;
+    }
+}
