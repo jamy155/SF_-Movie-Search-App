55d5dd49f8336eb3ae6df0a30d4650d7264e56ee
jamy155 <jarhudaugustin@gmail.com>
2025-04-22 19:04:41 -0400

    UPDATED moiveList to show movies

--- Movie App/force-app/main/default/lwc/moiveList/moiveList.js
+++ Movie App/force-app/main/default/lwc/moiveList/moiveList.js
@@ -1,10 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import  getMovies  from '@salesforce/apex/IMDBController.getMovies';
 
 export default class MoiveList extends LightningElement {
 
     enteredText='';
     searchText='';
     showText= 'Please enter Valid Movie Name';
+    movies=[];
 
     handleChange(event){
         this.enteredText = event.target.value;
@@ -12,5 +14,25 @@ export default class MoiveList extends LightningElement {
 
     handleClick(event){
         this.searchText = this.enteredText;
+    }
+
+    @wire(getMovies, { movieName: '$searchText' })
+    fetchMovies(result){
+
+        if(result.data){
+            let data=JSON.parse(result.data);  
+
+            if(data.success){
+                this.movies = data.result
+                this.showText = '';
+            }
+            else{
+                this.showText = 'Please enter Valid Movie Name';
+                this.movies = [];
+            }
+        } else if(result.error){
+                console.log('Error occurred while searching for movies'+result.error);
+                this.showText='Error occurred while searching for movies' + result.error;
+            }
     }
 }
